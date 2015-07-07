0ce38928fb324fe4f8ba789273ec52410cf91fc9
Envici Admin <enviciprojects@gmail.com>
2015-07-06 21:10:10 -0400

    Update 3

---
+++ import.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+md5 com.skumarspace.biteSMS2_0.0.1-109_iphoneos-arm.deb
+shasum /Users/SKumar/enviciprojects.github.io/debs/com.skumarspace.biteSMS2_0.0.1-109_iphoneos-arm.deb
+shasum -a 256 /Users/SKumar/enviciprojects.github.io/debs/com.skumarspace.biteSMS2_0.0.1-109_iphoneos-arm.deb
